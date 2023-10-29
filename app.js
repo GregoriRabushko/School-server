cdf7dbc766a570cd87937468ccccbb37b56e0805
Gregory <zak_zan@bk.ru>
2023-10-29 21:21:25 +0300

    first interaction with frontend

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-// const express = require("express");
-// const app = express();
-const http = require('http');
-const fs = require('fs');
-http.createServer((request, response) => {
-        console.log(`Запрошенный адрес ${request.url}`)
-        const filePath = request.url.substring(1);
-        fs.readFile(filePath, (err, data) =>{
-                if(err) {
-                        response.statusCode = 404;
-                        response.end('Not found resourse');
-                }
-                else{
-                        response.end(data);
-                }
-        })
-    }
-).listen(3000);
+const express = require("express");
+const app = express();
+// const urlencodedParser = express.urlencoded({extended: false});
+const jsonParser = express.json();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, PATCH, PUT, POST, DELETE, OPTIONS");
+    next();
+});
+app.post("/registration", jsonParser, (request, response) => {
+    // if (!request.body) return response.sendStatus(400);
+    // let userBody = request.body;
+    // let userInfo = JSON.parse(userBody)
+    // userInfo.id = '12';
+        console.log(request.body);
+        // console.log(userInfo);
+    // response.send(userInfo);
+});
+
+// app.post("/", urlencodedParser, (req, res) => {
+//     if (!req.body) return res.sendStatus(400);
+//     console.log(req.body);
+//     res.send(`${req.body.userName} - ${req.body.userAge}`);
+// });
+
+app.listen(3000);
 
